88bba7f776163605b188b2e0b568197fbb47b81e
IllyNozdrevatih <i.nozdrevatih@gmail.com>
2020-08-26 23:24:01 +0300

    fix bug

--- components/core/Header/Auth/index.vue
+++ components/core/Header/Auth/index.vue
@@ -1,12 +1,22 @@
 <template>
     <b-navbar-item tag="div">
         <div class="buttons">
-            <template v-if="$cookies.get('vuex').users.isAuth">
-                <button @click="$store.dispatch('users/logout')" class="button is-primary">
-                    <strong>Log out</strong>
-                </button>
+            <template v-if="$cookies.get('vuex')">
+                <template v-if="$cookies.get('vuex').users.isAuth">
+                    <button @click="$store.dispatch('users/logout')" class="button is-primary">
+                        <strong>Log out</strong>
+                    </button>
+                </template>
+                <template v-if="!$cookies.get('vuex').users.isAuth">
+                    <nuxt-link  to="/login" class="button is-primary">
+                        <strong>Sign up</strong>
+                    </nuxt-link>
+                    <a class="button is-light">
+                        Log in
+                    </a>
+                </template>
             </template>
-            <template v-if="!$cookies.get('vuex').users.isAuth">
+            <template v-if="!$cookies.get('vuex')">
                 <nuxt-link  to="/login" class="button is-primary">
                     <strong>Sign up</strong>
                 </nuxt-link>
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,14 @@
 export default function ({ $cookies, error }) {
-  const isAuth = $cookies.get('vuex').users.isAuth
-
-  if (!isAuth) {
+  if($cookies.get('vuex')) {
+    const isAuth = $cookies.get('vuex').users.isAuth
+  
+    if (!isAuth) {
+      error({
+        message: 'You are not connected',
+        statusCode: 403
+      })
+    }
+  } else {
     error({
       message: 'You are not connected',
       statusCode: 403
